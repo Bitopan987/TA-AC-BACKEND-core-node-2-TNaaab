95c7c2d7f8741414839a837da08d1065b862eb67
bitopan987 <bitopanxon1@gmail.com>
2021-07-29 22:41:07 +0530

    refactor

---
+++ block-BNaabe/node/server.js
@@ -0,0 +1,6 @@
+console.log(__filename);
+var path = require('path');
+var pathOfApp = path.join(__dirname, 'app.js');
+console.log(pathOfApp);
+var pathOfIndex = path.join(__dirname, 'index.html');
+console.log(pathOfIndex);
