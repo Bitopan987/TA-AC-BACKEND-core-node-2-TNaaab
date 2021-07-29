20a7c0f0571d29c0d02730d407ef8703d921d187
bitopan987 <bitopanxon1@gmail.com>
2021-07-29 18:40:16 +0530

    refactor

--- block-BNaaax/index.js
+++ block-BNaaax/index.js
@@ -1,6 +1,5 @@
-var relativePath = './form.html';
-// console.log(__dirname + './form.html');
+console.log(__dirname, __filename);
 
 var path = require('path');
 var pathName = path.join(__dirname, 'form.html');
-console.log(pathName, relativePath);
+console.log(pathName);
