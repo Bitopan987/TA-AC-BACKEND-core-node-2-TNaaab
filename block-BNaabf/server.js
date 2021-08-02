d14c4ca20ed5eb73988f8bf9d50d4dd019c3cf03
bitopan987 <bitopanxon1@gmail.com>
2021-08-02 18:07:17 +0530

    refactor

--- block-BNaabf/server.js
+++ block-BNaabf/server.js
@@ -7,7 +7,6 @@ function handleRequest(req, res) {
   req.on('data', (chunk) => {
     store += chunk;
   });
-
   req.on('end', () => {
     console.log(store);
     if (req.method === 'GET' && req.url === '/form') {
@@ -15,19 +14,17 @@ function handleRequest(req, res) {
       res.setHeader('Content-type', 'text/html');
       fs.createReadStream('./form.html').pipe(res);
     }
+    if (req.url === '/form' && req.method === 'POST') {
+      var dataObject = qs.parse(store);
+      console.log(dataObject);
+      res.setHeader('Content-type', 'text/html');
+      res.write(`<h2>${dataObject.name}</h2>`);
+      res.write(`<h3>${dataObject.email}</h3>`);
+      res.write(`<p>${dataObject.age}</p>`);
+      res.end();
+    }
   });
-
-  if (req.url === '/form' && req.method === 'POST') {
-    var dataObject = qs.parse(store);
-    console.log(dataObject);
-    res.setHeader('Content-type', 'text/html');
-    res.write(`<h2>${dataObject.name}</h2>`);
-    res.write(`<h3>${dataObject.email}</h3>`);
-    res.write(`<p>${dataObject.age}</p>`);
-    res.end();
-  }
 }
-
 server.listen(5678, () => {
   console.log('Listening to port 5678');
 });
